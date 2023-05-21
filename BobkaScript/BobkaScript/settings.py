788102fc3eb95595cf9229782b0d750591b76670
lucorus <106882669+lucorus@users.noreply.github.com>
2023-05-21 21:47:59 +0400

    another commit

--- BobkaScript/BobkaScript/settings.py
+++ BobkaScript/BobkaScript/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8wb(z8p9^zp5ww!f=p8-&+(d%$5_1@)m*dz!mkpo@f8%l15lr-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
-#ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
